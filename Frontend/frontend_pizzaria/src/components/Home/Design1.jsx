6d6c15bc719e59983c08c685dc48335828613212
Guptaverse <sggreat88@gmail.com>
2022-06-14 18:11:37 +0530

    err

--- Frontend/frontend_pizzaria/src/components/Home/Design1.jsx
+++ Frontend/frontend_pizzaria/src/components/Home/Design1.jsx
@@ -1,11 +1,57 @@
 import React from 'react'
+import '../../App.css' 
+
 
 const Design1 = () => {
-  return (
-    <div>
-        
+    // const objs =[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    // for(let index = 0; index < 16; index++) {
+    //    objs.push(<Circle/>)          
+    // //    objs.push("hi \n")          
+    // }
+    // return (
+    //  <div>
+    //     {/* { */}
+    //     // objs.map((e)=>{
+    //         // return <Circle/>
+    //     // })}
+    // </div>
+//   )
+
+
+
+return (
+    <div className='bg-black'>
+        sssss
     </div>
-  )
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+const Circle = ()=>{
+    return (
+        <div className="body h-4 w-4 rounded-full bg-gray-700">
+        </div>
+    )
+}
+
+
 
 export default Design1;
