7a0f8dbac9e341523e7bd2cca6916b14ccf47b88
Sumit Kumar Shakya <sumitshakya20101999@gmail.com>
2022-06-16 13:13:01 +0530

    update

--- Frontend/frontend_pizzaria/src/App.js
+++ Frontend/frontend_pizzaria/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Design1 from './components/Home/Design1';
+import Design1 from './components/Design1';
+import Temp from "./components/Temp"
 
 function App() {
   return (
-    <div className="App">
+    <div className="App h-28 bg-amber-500">
       <Design1/>
+      <Temp/>
     </div>
   );
 }
