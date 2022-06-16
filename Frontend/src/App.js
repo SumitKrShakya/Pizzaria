1b9afbec70c0f94448f1eaa85e2ae727aa65ce72
Guptaverse <sggreat88@gmail.com>
2022-06-16 14:49:12 +0530

    lal la laa la la tailwind installed

---
+++ Frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Test from './components/Test';
+import './global.css';
+const App = () => {
+  return (
+    <div className=' h-4 w-16 bg-blue-300'>
+        App
+        <Test/>
+    </div>
+  )
+}
+
+export default App
---
+++ Frontend/src/components/Test.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div className=' h-16 w-4 bg-gray-700'>
+        Test
+    </div>
+  )
+}
+
+export default Test
